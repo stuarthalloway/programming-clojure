d1bc2bd25ae001a2dd0f3a72851b72dc764c5596
Stuart Halloway <stu@thinkrelevance.com>
2009-01-02 17:33:08 -0500

    fixed runtests.sh to use embedded lib/ versions of all dependencies added ant to lib

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,1 @@
-java -cp $CLOJURE_HOME/clojure.jar:$CLOJURE_CONTRIB_HOME/clojure-contrib.jar:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-launcher.jar:. clojure.lang.Script examples/test.clj
+java -cp lib/clojure.jar:lib/clojure-contrib.jar:lib/ant.jar:lib/ant-launcher.jar:. clojure.lang.Script examples/test.clj
