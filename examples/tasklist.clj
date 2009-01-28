a3c829f5726c0ab1e565aff86a2e68eca8539a47
Stuart Halloway <stu@thinkrelevance.com>
2009-01-28 17:42:44 -0500

    completed tasklist example

--- examples/tasklist.clj
+++ examples/tasklist.clj
@@ -1,29 +1,37 @@
 (ns examples.tasklist
+  (:gen-class 
+   :extends org.xml.sax.helpers.DefaultHandler
+   :init init
+   :state state)
   (:use [clojure.contrib.duck-streams :only (reader)])
   (:import [java.io File]
            [org.xml.sax InputSource]
 	   [org.xml.sax.helpers DefaultHandler]
-	   [javax.xml.parsers SAXParserFactory])
-  (:gen-class
-   :extends org.xml.sax.helpers.DefaultHandler
-   :init init
-   :state state))
+	   [javax.xml.parsers SAXParserFactory]))
 
+; START: task-list
 (defn task-list [arg]
-  (let [handler (new examples.tasklist)]
+  (let [handler (new examples.tasklist)] ; <label id="code.tasklist.classname"/>
     (.. SAXParserFactory newInstance newSAXParser
 	(parse (InputSource. (reader (File. arg)))
 	       handler))
-    @(.state handler)))
+    @(.state handler))) ; <label id="code.tasklist.statename"/>
+; END: task-list
 
+; START: init
 (defn -init []
   [[] (atom [])])
+; END: init
 
+; START: startElement
 (defn -startElement
   [this uri local qname atts]
   (when (= qname "target")
-    (swap! (.state this) conj (.getValue atts "name"))))
+    (swap! (.state this) conj (.getValue atts "name")))) ; <label id="code.tasklist.update"/>
+; END: startElement
 
+; START: main
 (defn -main [& args]
   (doseq [arg args]
     (println (task-list arg))))
+; END: main
