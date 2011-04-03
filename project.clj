08e488fce507c3bb7882173ad4b7ae320de10fa1
asur <arusarka@gmail.com>
2011-04-04 03:26:48 +0530

    updated dependency versions + removed tracking files in lib

--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
 (defproject programming-clojure "1.2.0"
   :description "Examples from Programming Clojure"
   :dependencies [[org.clojure/clojure
-                  "1.2.0-master-SNAPSHOT"]
+                  "1.2.1"]
                  [org.clojure/clojure-contrib
-                  "1.2.0-master-SNAPSHOT"]
+                  "1.2.0"]
                  [compojure
-                  "0.3.2"]
+                  "0.6.2"]
                  [jline
                   "0.9.94"]
                  [hsqldb
                   "1.8.0.10"]]
-  :dev-dependencies [[autodoc "0.7.0"]
-                     [swank-clojure "1.1.0"]]
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
-                 "sesame" "http://repo.aduna-software.org/maven2/releases"})
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
+                     [swank-clojure "1.3.0"]])
