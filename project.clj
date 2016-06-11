ec96d8d556bfad45c923791e1a93c64db4c228d1
Stuart Halloway <stu@cognitect.com>
2016-06-11 13:07:56 -0400

    upgrade to 1.7.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject programming-clojure "1.3.0"
   :description "Examples from Programming Clojure"
   :aot [examples.tasklist]
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.0.6"]
                  [org.clojure/test.generative "0.1.2"]
                  [ring "0.3.11" :exclusions [org.clojure/clojure
