f463bd045f4a60105ca3a5a7d941119ae6264027
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2021-01-13 14:43:43 +0900

    add comment

--- my-study/chapter-1/1-2.clj
+++ my-study/chapter-1/1-2.clj
@@ -2,10 +2,13 @@
 
 (def visitors (atom #{}))
 
+; atom の状態を書き換える。
 (swap! visitors conj "Stu")
 
+; atom の状態を参照できる
 @visitors
 
+; 上の２つを使えば、状態を保持できる関数ができる。
 (defn hello
     "Writes hello message to *out*. Calls you by username.
     Knows if you have been here before."
